c39d9477f205e2b445aa6223cf5d993516193626
Harshh2409 <89562262+Harshh2409@users.noreply.github.com>
2021-11-14 14:42:13 +0530

    Create home.php
    home page after login

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="style.css" rel="stylesheet"/>
+</head>
+<body>
+    <h1 id="heading">WELCOME TO H-MART</h1>
+    <hr>
+    <a id="cashreg" href="https://cash-register-harshit.netlify.app/">Cash Register</a>
+    <a id="sale" href="">Check Sale Made</a>
+    <a id="editprofile" href="">Edit your profile</a>
+    <a id="logout" href="logout.php">Log out</a>
+    <hr>
+    <h4 id="heading3">About</h4>
+    <div id="about">Welcome to H-Mart employee application. Log in to access your profile</div>
+</body>
+</html>
